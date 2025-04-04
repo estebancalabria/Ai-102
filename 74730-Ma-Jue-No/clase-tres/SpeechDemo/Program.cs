130749573ecc9d245e4ccb251f06f48e4ea917b6
Esteban Calabria <esteban.calabria@gmail.com>
2025-04-03 22:04:41 -0300

    clase-tres

---
+++ 74730-Ma-Jue-No/clase-tres/SpeechDemo/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.CognitiveServices.Speech;
+
+String key = "xxxxxx";
+String region = "eastus";
+
+
+//
+//Ejemplo Sintesis de voz
+//
+
+/*SpeechConfig config = SpeechConfig.FromSubscription(key, region);
+//Defino el nombre de la voz que quiero
+//config.SpeechSynthesisVoiceName = "es-ES-TristanMultilingualNeural";
+config.SpeechSynthesisVoiceName = "es-MX-DaliaNeural";
+
+SpeechSynthesizer synthesizer = new SpeechSynthesizer(config);
+
+await synthesizer.SpeakTextAsync("Hola");
+await synthesizer.SpeakTextAsync("Como estas?");
+
+Console.WriteLine("Press any key to exit...");  
+Console.ReadKey();*/
+
+
+//
+// Ejemplo Reconocimiento de Voz
+//
+SpeechConfig config = SpeechConfig.FromSubscription(key, region);
+config.SpeechRecognitionLanguage = "es-AR";
+
+SpeechRecognizer speechRecognizer = new SpeechRecognizer(config);
+Console.WriteLine("Habla nomas....");
+SpeechRecognitionResult result = await speechRecognizer.RecognizeOnceAsync();
+
+if (result.Reason == ResultReason.RecognizedSpeech)
+{
+    Console.WriteLine($"Texto reconocido: {result.Text}");
+}
+else 
+{
+    Console.WriteLine("No se reconoció nada.");
+}
+
+Console.WriteLine("Press any key to exit...");  
+Console.ReadKey();
---
+++ 74730-Ma-Jue-No/clase-tres/TranslatorSpeechDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Translation;
+
+String key = "6HrZWqr7Dejthnfx1Jr1Wfaxtxu2IvhQmYlG84K3S8VQBW3ADX0mJQQJ99BDACYeBjFXJ3w3AAAYACOGYMQs";
+String region = "eastus";
+
+SpeechTranslationConfig config = SpeechTranslationConfig.FromSubscription(key, region);
+config.SpeechRecognitionLanguage = "es-ES"; // The language you want to recognize
+config.AddTargetLanguage("pt"); // The language you want to translate to
+config.AddTargetLanguage("en"); // The language you want to translate to
+
+TranslationRecognizer recognizer = new TranslationRecognizer(config);
+
+Console.WriteLine("Speak into your microphone.");
+
+TranslationRecognitionResult  result = await recognizer.RecognizeOnceAsync();
+
+if (result.Reason == ResultReason.TranslatedSpeech)
+{
+    Console.WriteLine($"Recognized: {result.Text}");
+    Console.WriteLine();
+    Console.WriteLine($"Translated to Portugues: {result.Translations["pt"]}");
+    Console.WriteLine();
+    Console.WriteLine($"Translated to Ingles: {result.Translations["en"]}");
+}
+else
+{
+    Console.WriteLine("No speech could be recognized.");
+}
+
+Console.WriteLine("Press any key to exit.");
+Console.ReadKey();
